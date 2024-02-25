221632ba9a264f746e12665c46829f82caaf43e1
Diego <dtellez95@gmail.com>
2024-02-25 11:40:35 +0000

    create reusable axios instance

---
+++ src/axios.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance } from 'axios';
+
+type TranslationResponse = {
+  translation: string;
+}
+// ref: https://github.com/dtellz/languagewire-api/blob/master/server/exceptions/handler.py
+type TranslationError = {
+    status: 'error';
+    statusCode: number;
+    errorType?: string; // Optional, as it might not be present in all errors
+    message: string;
+};
+
+const baseURL = 'http://localhost:8000';
+
+// Setting withCredentials at the Axios instance level
+const APICall: AxiosInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export { APICall, type TranslationResponse, type TranslationError };
